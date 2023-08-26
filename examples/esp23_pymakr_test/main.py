e77ba1feabe9de1756327df3d2ed0fee677e1692
Anton Vanhoucke <antonvh@gmail.com>
2023-08-26 17:27:56 +0200

    added esp32 pymakr demo

---
+++ examples/esp23_pymakr_test/main.py
@@ -0,0 +1,19 @@
+# main.py -- put your code here!
+print(1+2)
+print("Hello World!")
+from pupremote import PUPRemoteSensor, ESP32, SPIKE_ULTRASONIC
+
+def msg(txt):
+    print(txt)
+    return txt+txt
+
+def num(n):
+    print(n)
+    return 2*n
+
+p=PUPRemoteSensor(sensor_id=SPIKE_ULTRASONIC, platform=ESP32)
+p.add_command('msg',"repr","repr")
+p.add_command('num',from_hub_fmt="b", to_hub_fmt="b")
+
+while(True):
+    connected=p.process()
