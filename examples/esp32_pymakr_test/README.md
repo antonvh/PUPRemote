4df91b9f9528b7d8b6ab4ebb0ea99fa284de89ff
Anton Vanhoucke <antonvh@gmail.com>
2023-08-27 01:06:55 +0200

    add pybricks code and readme

---
+++ examples/esp32_pymakr_test/README.md
@@ -0,0 +1,10 @@
+# How to run this demo #
+
+- In VS Code Install the Pymakr PREVIEW extension
+- In the PyMakr:Projects accordeon click ADD DEVICE and select your ESP32
+- Start Pymakr Development mode
+
+- In pybricks create a new file called pupremote
+- paste the contents of pupremote.py
+- create a new file called pybricks_code
+- paste the contents of pybricks_code.py
---
+++ examples/esp32_pymakr_test/pybricks_code.py
@@ -0,0 +1,21 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from pybricks.iodevices import PUPDevice
+from pupremote import PUPRemoteHub
+
+
+p=PUPRemoteHub(Port.A)
+
+p.add_command('msg',"repr","repr")
+p.add_command('num',from_hub_fmt="b", to_hub_fmt="b")
+
+# Play with the delay (default 100) to see when then answer is actual
+DELAY = 10
+print( p.call('msg','kello', wait_ms=DELAY), '== kellokello' )
+print( p.call('num',5, wait_ms=DELAY), '== 10' )
+print( p.call('msg','hello', wait_ms=DELAY), '== hellohello' )
+print( p.call('num',4, wait_ms=DELAY), '== 8' )
+
