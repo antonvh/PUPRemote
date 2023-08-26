a0221d3dbf5d567ab01e1213f153091ca6dbd7ce
Anton Vanhoucke <antonvh@gmail.com>
2023-08-27 01:21:53 +0200

    Demo of channel to share variable

--- examples/esp32_pymakr_test/main.py
+++ examples/esp32_pymakr_test/main.py
@@ -6,19 +6,31 @@ if 'lpf2' in sys.modules:
     del sys.modules['lpf2']
 
 from pupremote import PUPRemoteSensor, ESP32, SPIKE_ULTRASONIC
-
+from time import sleep_ms
+counter = 0
 
 def msg(txt):
+    global counter
+    counter += 1
     print(txt)
     return txt+txt
 
 def num(n):
+    global counter
+    counter += 1
     print(n)
     return 2*n
 
+# def cntr():
+#     global counter
+#     return counter
+
 p=PUPRemoteSensor(sensor_id=SPIKE_ULTRASONIC, platform=ESP32)
+p.add_channel('cntr',to_hub_fmt="b")
 p.add_command('msg',"repr","repr")
 p.add_command('num',from_hub_fmt="b", to_hub_fmt="b")
 
 while(True):
     connected=p.process()
+    p.update_channel('cntr', counter)
+    sleep_ms(20)
--- examples/esp32_pymakr_test/pybricks_code.py
+++ examples/esp32_pymakr_test/pybricks_code.py
@@ -9,13 +9,18 @@ from pupremote import PUPRemoteHub
 
 p=PUPRemoteHub(Port.A)
 
+
+p.add_channel('cntr',to_hub_fmt="b") # Counts the number of msg and num calls
 p.add_command('msg',"repr","repr")
 p.add_command('num',from_hub_fmt="b", to_hub_fmt="b")
 
 # Play with the delay (default 100) to see when then answer is actual
-DELAY = 10
+DELAY = 30
+print( p.call('cntr') )
 print( p.call('msg','kello', wait_ms=DELAY), '== kellokello' )
 print( p.call('num',5, wait_ms=DELAY), '== 10' )
 print( p.call('msg','hello', wait_ms=DELAY), '== hellohello' )
 print( p.call('num',4, wait_ms=DELAY), '== 8' )
+print( p.call('num',0, wait_ms=DELAY), '== 0' )
+print( p.call('cntr') )
 
