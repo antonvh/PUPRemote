dd012415428bf5b3c790ddfcfb516e3f52d48cdd
Anton Vanhoucke <antonvh@gmail.com>
2023-10-25 22:27:35 +0200

    Working esp32_dummy_daya

--- examples/esp32_dummy_data/pup_demo_esp32.py
+++ examples/esp32_dummy_data/pup_demo_esp32.py
@@ -1,44 +1,60 @@
-# Copy this file to OpenMV Cam H7 Plus
-# Be sure to also copy pupremote.py and lpf2.py to the camera drive
+# Copy this file to the LMS-ESP32 with Thonny and name it main.py
+# Copy pupremote.py and lpf2.py to the LMS-ESP32 with Thonny
+# Connect the LMS-ESP32 to the SPIKE Prime hub though port A
+# Run this program on the LMS-ESP32 by rebooting it (ctrl-D in Thonny)
+
 
 ### Setup pupremote code
 from pupremote import  PUPRemoteSensor, SPIKE_ULTRASONIC
-value=1
+
 def msg(*argv):
     if argv!=():
         print(argv)
     return str(value)
 
+value=0
 def num(*argv):
+    global value
     if argv!=():
         print(argv)
-    return 2*value,3*value,4*value
+    else:
+        print("num called without args")
+    value += 1
+    return 2*value,-3*value,4*value
 
+sm_data=None
 def smalldata(*argv):
+    global sm_data
     if argv!=():
         print(argv)
-    
-    return 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+        sm_data = argv
+    else:
+        print("smalldata() called without args")
+    return sm_data
 
+# 32 bytes crashes the connection somehow.
+lg_data=tuple(range(32))
 def largedata(*argv):
+    global lg_data
     if argv!=():
         print(argv)
+        lg_data = argv
+    else:
+        print("largedata() called without args")
     
-    return 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,\
-           1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+    return lg_data
 
 
 p=PUPRemoteSensor(sensor_id=SPIKE_ULTRASONIC)
 p.add_command('msg',"repr","repr")
 p.add_command('num',from_hub_fmt="3b", to_hub_fmt="3b")
-p.add_command('smalldata',from_hub_fmt="16b", to_hub_fmt="16b")
-p.add_command('largedata',from_hub_fmt="32b", to_hub_fmt="32b")
+p.add_command('smalldata',from_hub_fmt="16B", to_hub_fmt="16B")
+p.add_command('largedata',from_hub_fmt="32B", to_hub_fmt="32B")
 ### End of pupremote setup code
 
 ### Main loop
 while(True):
     connected=p.process()
-    value+=1
 
 
 
