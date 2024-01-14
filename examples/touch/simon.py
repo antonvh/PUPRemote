571ebada69e7cb0aa81fda9b76d9f9d04816e172
Ste7an <51531682+ste7anste7an@users.noreply.github.com>
2024-01-14 23:21:07 +0100

    added sounds for loosing and starting game

--- examples/touch/simon.py
+++ examples/touch/simon.py
@@ -14,7 +14,7 @@ buttons={'y':(3,[4,4],'C#4/16'),'g':(4,[4,0],'E3/16'),'r':(6,[0,0],'A4/16'),'b':
 game=[]
 level=0
 holdoff=0
-
+highscore=0
 def show(col):
     hub.display.pixel(*buttons[col][1],100)
     hub.speaker.play_notes([buttons[col][2]])
@@ -39,13 +39,45 @@ p.add_command('touch','10B')
 
 hub.speaker.volume(20)
 gameover=False
+#gameover=True
+level=0
 while True:
     if gameover:
+        hub.speaker.play_notes(["C5/8","B4/8","A#4/8","A4/8","G#4/4"])
+        wait(1000)
+        tens=level//10
+        for t in range(tens):
+            hub.display.pixel(0,0,100)
+            hub.display.pixel(0,4,100)
+            hub.display.pixel(4,0,100)
+            hub.display.pixel(4,4,100)
+            wait(200)
+            hub.display.off()
+            wait(200)
+        ones=level%10
+        for o in range(ones):
+            hub.display.pixel(4,0,100)
+            wait(200)
+            hub.display.off()
+            wait(200)
+        wait(1000)
         gameover=False
         game=[]
         level=0
-    
-    for l in range(10):
+    start=0
+    while not start:
+        hub.display.pixel(4,0,100)
+        btns=p.call('touch')
+        col=get_key_col(btns)
+        start=col=='g'
+        wait(100)
+        hub.display.pixel(4,0,0)
+        wait(100)
+    wait(500)
+    hub.speaker.play_notes(["G4/8","C5/8","F5/8"])
+    wait(500)
+            
+    while not gameover:
         col_new=next()
         for g in game:
             show(g)
@@ -62,11 +94,14 @@ while True:
                 wait(20)
             hub.display.off()
             show(col)
+            wait(100)
             if col!=game[i]:
                 print('game over')
                 gameover=True
                 break
-        wait(1000)
+        wait(200)
+        hub.display.off()
+        wait(800)
         if gameover:
             break       
             
